9d293f6adabaf992f9cc59d0b42e10d095efb0ec
filipe <filipegustavofglv@gmail.com>
2020-01-17 13:52:04 -0300

    Commit aula 11

---
+++ src/app/pages/categoria/categoria-list/categoria-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-list',
+  templateUrl: './categoria-list.component.html',
+  styleUrls: ['./categoria-list.component.scss']
+})
+export class CategoriaListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('Caiu em categoria list');
+  }
+
+}
