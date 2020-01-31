cb2bd126f5247c5bb9f4d729e32d061a0cead126
filipe <filipegustavofglv@gmail.com>
2020-01-31 15:04:49 -0300

    improving reports

--- src/app/pages/relatorio/relatorio.module.ts
+++ src/app/pages/relatorio/relatorio.module.ts
@@ -3,13 +3,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { RelatorioRoutingModule } from './relatorio-routing.module';
 import { RelatorioComponent } from './relatorio/relatorio.component';
-
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [RelatorioComponent],
   imports: [
     SharedModule,
     RelatorioRoutingModule,
+    ChartModule
   ]
 })
 export class RelatorioModule { }
